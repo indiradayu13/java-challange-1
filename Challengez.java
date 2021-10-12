8a49113058ece21dc3e255fb90f3890b2b18df29
indira <91586302+indiradayu13@users.noreply.github.com>
2021-10-12 22:14:42 +0700

    Create Challengez.java

---
+++ Challengez.java
@@ -0,0 +1,22 @@
+public class Challengez {
+    public static void main(String[] args){
+    String firstName = "ida";
+    String lastName = "Herdasari";
+    String secondName = "Ayu";
+    String thirdName = "Indira";
+    int age = 19;
+    String address = "Jl. Menteng VII";
+    String phoneNumber = "095267315767";
+    String hobby = "makan";
+    
+    System.out.println("first name : " + firstName);
+    System.out.println("last name: " + lastName);
+    System.out.println("second name : " + secondName);
+    System.out.println("third name : " + thirdName);
+    System.out.println("Age : "  + age);
+    System.out.println("Address : " + address);
+    System.out.println("Phone Number :" + phoneNumber);
+    System.out.println("Hobby :" + hobby);
+
+}
+}
